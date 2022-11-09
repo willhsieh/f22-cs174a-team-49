80e2b46d1aa843f867a8da36ac600c1f0daf040d
William Hsieh <44691029+willhsieh@users.noreply.github.com>
2022-11-09 14:02:17 -0800

    rebrand

--- tinymarbles/tinymarbles.js
+++ tinymarbles/tinymarbles.js
@@ -4,7 +4,7 @@ const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
 } = tiny;
 
-export class Assignment3 extends Scene {
+export class TinyMarbles extends Scene {
     constructor() {
         // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
         super();
